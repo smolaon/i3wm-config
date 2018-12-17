b996c29e69f4b72a72ec614170e286eab3adb0f2
Ondrej Smola <ondrej.smola@tcpcloud.eu>
2018-12-17 18:10:13 +0100

    changed rofi command

---
+++ install.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+apt update && apt install i3blocks compton rofi feh
+
+wget https://github.com/supermarin/YosemiteSanFranciscoFont/raw/master/System%20San%20Francisco%20Display%20Bold.ttf -P ~/.fonts/
+wget https://github.com/supermarin/YosemiteSanFranciscoFont/raw/master/System%20San%20Francisco%20Display%20Regular.ttf -P ~/.fonts/
+wget https://github.com/supermarin/YosemiteSanFranciscoFont/raw/master/System%20San%20Francisco%20Display%20Thin.ttf -P ~/.fonts/
+wget https://github.com/supermarin/YosemiteSanFranciscoFont/raw/master/System%20San%20Francisco%20Display%20Ultralight.ttf -P ~/.fonts/
