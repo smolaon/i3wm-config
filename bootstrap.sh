a4f963d169bafb36121bd6efc9abd1c19dca5c6d
Ondrej Smola <ondrej.smola@tcpcloud.eu>
2019-05-05 11:13:57 +0200

    added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+sudo apt install i3-wm i3blocks
+sudo apt install vim
+sudo apt install terminator chromium-browser blueman
+
+#Slack
+
+#vscode
+
+#docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+sudo systemctl enable docker
+
+#kubectl
+
+#gcloud
+
+#spotify
+# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 3. Update list of available packages
+sudo apt-get update
+
+# 4. Install Spotify
+sudo apt-get install spotify-client
+
+
